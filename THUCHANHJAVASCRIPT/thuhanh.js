1cc16d954e2faf011a97cb12d201cead82ac6d64
ritakool <jokernguyen1994@gmail.com>
2023-02-23 14:45:14 +0700

    thuchanh

---
+++ THUCHANHJAVASCRIPT/thuhanh.js
@@ -0,0 +1,6 @@
+function showMessage(){
+    alert('đừng cố nhấp');
+    document.write('Xin chào pạnnnnnnnnnnn');
+    // document.getElementById('MessageDiv').innerHTML='Mất tiền rồi nhé';
+    // console.log('Xin chào!);
+}
